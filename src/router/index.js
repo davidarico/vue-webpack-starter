d6dd59ce8d8cf39de299ccc24b638f4a19f1cb7a
David Arico <davidarico87@gmail.com>
2021-10-04 18:15:04 -0700

    convert to a basic webpack

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
-import Journal from '../views/Journal.vue'
-import Cooking from '../views/Cooking.vue'
-import Training from '../views/Training.vue'
 
 Vue.use(VueRouter);
 
@@ -12,21 +9,6 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
-  },
-  {
-    path: "/journal",
-    name: "journal",
-    component: Journal,
-  },
-  {
-    path: "/cooking",
-    name: "cooking",
-    component: Cooking,
-  },
-  {
-    path: "/training",
-    name: "training",
-    component: Training,
   }
 ];
 
