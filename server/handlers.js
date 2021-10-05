2d80c34016a33f897d22389f4f3f8bcbeeb87865
David Arico <davidarico87@gmail.com>
2021-10-04 18:20:39 -0700

    add test api call

---
+++ server/handlers.js
@@ -0,0 +1,7 @@
+const test = (req, res) => {
+    res.status(200).send('this is a test')
+}
+
+module.exports = {
+    test
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const app = require('./app')
+const handlers = require('./handlers')
+
+app.get('/test', handlers.test)
 
 app.listen().catch((err) => {
   console.log(err.toString())
